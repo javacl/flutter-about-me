8ff0879afc0ac5b8a58c0df4555b0ff6349e4278
Javad <javacl.sadeghi@gmail.com>
2025-08-26 11:34:03 +0330

    created project

--- packages/app/android/app/build.gradle.kts
+++ packages/app/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "flutter.about.me.flutter_about_me"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
